63155853dc10c19d06920fcf6ec6790ec2fa6ef5
vinay kumar <vinay.kumar@impressico.com>
2021-04-24 16:33:44 +0530

    'init-commit'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/view/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
